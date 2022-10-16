8570222248336f0f4a88003afd936712316f92eb
uriel46 <sampsonuriel@gmail.com>
2022-10-15 18:10:32 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	putchar('\n');
+	return (0);
+}
