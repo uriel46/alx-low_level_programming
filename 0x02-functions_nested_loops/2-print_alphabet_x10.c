df148f92ee91c1a18bf29b620eea822a9066e4ca
uriel46 <sampsonuriel@gmail.com>
2022-10-18 15:57:25 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this prints the alphabet ten times
+ */
+
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (la = ’a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
