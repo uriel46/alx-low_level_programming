0ebd31f87c0377bec5988a688817914e5df563ff
uriel46 <sampsonuriel@gmail.com>
2022-10-19 05:55:03 -0700

    #7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,12 @@
 
 int print_last_digit(int n)
 {
-	int x;
+	int ld = n % 10;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
 }
