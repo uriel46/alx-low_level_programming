9c202d5216a7ba0f627a9a7f19969bfeb1af4eab
uriel46 <sampsonuriel@gmail.com>
2022-11-02 22:41:19 -0700

    finally

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,14 +2,14 @@
 
 int strlen_no_wilds(char *str);
 void literate_wild(char **wildstr);
-char *postfix_match(char *str char *postfix);
+char *postfix_match(char *str, char *postfix);
 int wildcmp(char *s1, char *s2);
 
 /**
  * strlen_no_wilds - Return the length of a string
  * ignoring wildcard characters
  * @str: the string to ne measured
- * Return; the length
+ * Return: the length
  */
 int strlen_no_wilds(char *str)
 {
